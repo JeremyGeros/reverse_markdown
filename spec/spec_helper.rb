34e5efdb8a0a5ffc226c17b8c1f5536c461cefc7
Jeremy Wells <jemmyw@gmail.com>
2018-11-03 00:52:43 +1300

    Remove CodeClimate references due to updated gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,5 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-
 require 'simplecov'
-# require 'byebug'
+require 'byebug'
 
 SimpleCov.adapters.define 'gem' do
   add_filter '/spec/'
