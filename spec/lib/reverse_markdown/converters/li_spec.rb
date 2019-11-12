b58cdc03fdb8d5947574db99a76c1a61c285f19a
Zach Schneider <zach@schneid.io>
2019-11-12 15:39:50 -0500

    Fix specs from master merge

--- spec/lib/reverse_markdown/converters/li_spec.rb
+++ spec/lib/reverse_markdown/converters/li_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ReverseMarkdown::Converters::Li do
   it 'does not fail without a valid parent context' do
     input = Nokogiri::XML.parse("<li>foo</li>").root
-    result = ReverseMarkdown.convert(input, 0)
+    result = ReverseMarkdown.convert(input)
     expect(result).to eq "- foo\n"
   end
 end
